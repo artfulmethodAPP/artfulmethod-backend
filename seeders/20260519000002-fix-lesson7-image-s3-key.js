cc662aecc70c9b443dc2870bf0e283b4d3dda7df
Hamza Ali <48332435+hamzaali4943@users.noreply.github.com>
2026-05-19 19:42:05 +0500

    Fix Lesson 7 image S3 key in seeder

--- seeders/20260519000002-fix-lesson7-image-s3-key.js
+++ seeders/20260519000002-fix-lesson7-image-s3-key.js
@@ -4,7 +4,7 @@ module.exports = {
   async up(queryInterface) {
     await queryInterface.sequelize.query(
       `UPDATE Lesson_Contents
-       SET image_s3_key = 'test-uploads/5d421ad3-3173-4f03-ac84-a3fe3f9ca87e.jpg'
+       SET image_s3_key = 'test-uploads/93477d71-b674-45c9-82e1-e4c872ac82c6.jpg'
        WHERE course_lesson_id = 7`,
     );
   },
@@ -12,7 +12,7 @@ module.exports = {
   async down(queryInterface) {
     await queryInterface.sequelize.query(
       `UPDATE Lesson_Contents
-       SET image_s3_key = 'test-uploads/93477d71-b674-45c9-82e1-e4c872ac82c6.jpg'
+       SET image_s3_key = 'test-uploads/5d421ad3-3173-4f03-ac84-a3fe3f9ca87e.jpg'
        WHERE course_lesson_id = 7`,
     );
   },
